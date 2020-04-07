0f1d3959be9a011a4c496ae368adb4a41032adc1
AmrDeveloper <amr96@programmer.net>
2020-04-07 10:06:58 +0200

    Add more icons and set hover text for every icon

--- src/snapmacro/controllers/MainController.java
+++ src/snapmacro/controllers/MainController.java
@@ -13,16 +13,32 @@ public class MainController implements Initializable {
 
     //Actions
     @FXML private ImageView runAction;
+    @FXML private ImageView debugAction;
+    @FXML private ImageView restartAction;
+    @FXML private ImageView loadAction;
+    @FXML private ImageView saveAction;
+    @FXML private ImageView cursorAction;
 
+    //Layouts
     @FXML private TabPane scriptTabPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setupViewsHover();
-
+        setViewsListeners();
     }
 
     private void setupViewsHover(){
         Tooltip.install(runAction, new Tooltip("Run snap Script"));
+        Tooltip.install(debugAction, new Tooltip("Debug snap Script"));
+        Tooltip.install(restartAction, new Tooltip("Restart snap Script"));
+        Tooltip.install(loadAction, new Tooltip("Load snap file"));
+        Tooltip.install(saveAction, new Tooltip("Save snap File"));
+        Tooltip.install(cursorAction, new Tooltip("Show cursor Position"));
+    }
+
+    private void setViewsListeners(){
+        runAction.setOnMouseClicked(e -> System.out.println("Run script"));
+
     }
 }
