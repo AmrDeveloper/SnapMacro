daedab65a76311eb00327a4270543b18af28d8e8
AmrDeveloper <amr96@programmer.net>
2020-04-07 09:14:27 +0200

    Add new Icons

--- src/snapmacro/Main.java
+++ src/snapmacro/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/main_view.fxml"));
-        primaryStage.setTitle("SnapMacro");
+        primaryStage.setTitle("Snap Macro");
         Scene scene = new Scene(root,600, 400);
         scene.getStylesheets().add("snapmacro/styles/editor_style.css");
 
--- src/snapmacro/controllers/MainController.java
+++ src/snapmacro/controllers/MainController.java
@@ -3,16 +3,26 @@ package snapmacro.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
+    //Actions
+    @FXML private ImageView runAction;
+
     @FXML private TabPane scriptTabPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        setupViewsHover();
 
+    }
+
+    private void setupViewsHover(){
+        Tooltip.install(runAction, new Tooltip("Run snap Script"));
     }
 }
