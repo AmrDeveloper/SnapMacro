87dcafc9230c3131d81d6d098e919e087505236f
AmrDeveloper <amr96@programmer.net>
2020-04-08 14:08:35 +0200

    Implement Dark Theme and rebuild jar

--- src/snapmacro/Main.java
+++ src/snapmacro/Main.java
@@ -13,7 +13,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/main_view.fxml"));
         Scene scene = new Scene(root,600, 400);
+
+        //Bind Styles
+        scene.getStylesheets().add("snapmacro/styles/main_style.css");
+        scene.getStylesheets().add("snapmacro/styles/tabs_style.css");
         scene.getStylesheets().add("snapmacro/styles/editor_style.css");
+        scene.getStylesheets().add("snapmacro/styles/terminal_style.css");
 
         Image appIconImage = new Image(Main.class.getResourceAsStream("res/app_icon.png"));
         primaryStage.setTitle("Snap Macro");
